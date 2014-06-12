d5377206daf33cd26aa70253046a8ad2282bd1d1
kekkaishi <754283149@qq.com>
2014-06-13 00:23:33 +0800

    Revert "Yiicms"
    This reverts commit 5920f7254aeddb8775be32d7cdf65b8f5c027f05.

--- cms/protected/models/Goods.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Goods extends CActiveRecord{
-	public static function model($className=__CLASS__){
-		return parent::model($className);
-	}
-	public function tableName(){
-		return '{{essay}}';
-	}
-}
